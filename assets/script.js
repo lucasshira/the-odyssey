8362c1ffd444aa0c138641956915094e1f38fa3d
Lucas Gabriel <144144880+lucasshira@users.noreply.github.com>
2024-01-31 16:59:10 -0300

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,6 @@
 // const APIKEY = '1d4a1fe898c5b10f6f4ce16450f89761';
-const APIURL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=1d4a1fe898c5b10f6f4ce16450f89761&page=1';
+const APIURL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=1d4a1fe898c5b10f6f4ce16450f89761&page=';
+
 const IMGPATH = 'https://image.tmdb.org/t/p/w1280';
 const SEARCHAPI = 'https://api.themoviedb.org/3/search/movie?&api_key=1d4a1fe898c5b10f6f4ce16450f89761&query=';
 
@@ -21,16 +22,44 @@ const mainMenu = document.querySelector('.mainMenu');
 const openMenu = document.querySelector('.openMenu');
 const closeMenu = document.querySelector('.closeMenu');
 
-let bookmarked = false;
+const prevPageButton = document.getElementById('prevPage');
+const nextPageButton = document.getElementById('nextPage');
+let currentPage = 1;
+
+async function getMovies(url, page) {
+    return fetch(`${url}${page}`)
+        .then(response => response.json())
+        .then(data => {
+            console.log('Search Results:', data.results);
+            return data.results;
+        })
+        .catch(error => {
+            console.error('Error fetching movies:', error);
+            return [];
+        });
+}
+
+async function searchMovies(query, page) {
+    const url = `${SEARCHAPI}${encodeURIComponent(query)}&page=${page}`;
+    return fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            console.log('Search Results:', data.results);
+            return data.results;
+        })
+        .catch(error => {
+            console.error('Error fetching search results:', error);
+            return [];
+        });
+}
 
 function showHomePage() {
-    getMovies(APIURL);
+    fetchMovies(APIURL, currentPage);
 
-    async function getMovies(url) {
-        const resp = await fetch(url);
-        const respData = await resp.json();
-    
-        showMovies(respData.results);
+    async function fetchMovies(url, page) {
+        console.log(`Fetching movies for page ${page}...`);
+        const movies = await getMovies(url, page);
+        showMovies(movies);
     }
     
     function showMovies(movies) {
@@ -47,12 +76,37 @@ function showHomePage() {
             movieEl.innerHTML = `
                 <img src="${IMGPATH + poster_path}" alt="${title}">
                 <div class="overview">
-                <h3>${title}</h3>
                 <p class="expand"><i class="fa-solid fa-maximize"></i> Expand</p>
+                <h3>${title}</h3>
                 </div>
             `;
 
+            const addToFavoritesButton = document.createElement('button');
+            const addToFavoritesIcon = document.createElement('i');
+            addToFavoritesIcon.classList.add('fa', 'fa-bookmark', 'fa-regular');
+            addToFavoritesButton.style.display = 'none';
+            
+            addToFavoritesButton.dataset.movieId = movie.id;
+            
+            addToFavoritesButton.appendChild(addToFavoritesIcon);
+            
+            addToFavoritesButton.addEventListener('click', (e) => {
+                e.stopPropagation();
+                saveToFavorites(movie);
+                updateFavoriteIcon(movie.id);
+                addToFavoritesIcon.classList.add('fa', 'fa-bookmark', 'fa-solid');
+            });
+            
+            movieEl.appendChild(addToFavoritesButton);
             main.appendChild(movieEl);
+
+            movieEl.addEventListener('mouseenter', () => {
+                addToFavoritesButton.style.display = 'flex';
+            });
+        
+            movieEl.addEventListener('mouseleave', () => {
+                addToFavoritesButton.style.display = 'none';
+            });
 
             movieEl.addEventListener('click', () => {
                 const movieId = movie.id;
@@ -61,11 +115,18 @@ function showHomePage() {
         });
     }
     
-    function performSearch() {
+    async function performSearch() {
         const searchTerm = search.value;
     
         if (searchTerm) {
-            getMovies(SEARCHAPI + searchTerm);
+            try {
+                currentPage = 1;
+                const movies = await searchMovies(searchTerm, currentPage);
+                showMovies(movies);
+            } catch (error) {
+                console.error('Error fetching search results:', error);
+            }
+    
             search.value = '';
         }
     }
@@ -80,6 +141,18 @@ function showHomePage() {
         performSearch();
     });
 }
+
+prevPageButton.addEventListener('click', () => {
+    if (currentPage > 1) {
+        currentPage--;
+        showHomePage();
+    }
+});
+
+nextPageButton.addEventListener('click', () => {
+    currentPage++;
+    showHomePage();
+});
 
 function openMovieModal(movieId, voteAverage, overview) {
     const castInfoPromise = fetch(`https://api.themoviedb.org/3/movie/${movieId}/credits?api_key=1d4a1fe898c5b10f6f4ce16450f89761&query=`)
@@ -108,7 +181,6 @@ function openMovieModal(movieId, voteAverage, overview) {
                         profilePath: directorData.profile_path
                     };
                 });
-
             return Promise.all([Promise.all(actorsInfo), directorInfo]);
         });
 
@@ -174,16 +246,52 @@ function openMovieModal(movieId, voteAverage, overview) {
             modal.style.display = 'block';
         })
         .catch(error => {
-            console.error('Erro ao buscar dados do filme:', error);
+            console.error('Erro ao buscar dados do filme: ', error);
         });
 }
 
 function saveToFavorites(movie) {
     const favorites = JSON.parse(localStorage.getItem('favoritesMovie')) || [];
     const movieExists = favorites.some((favMovie) => favMovie.id === movie.id);
+    const tooltip = document.createElement('span');
+
+    tooltip.classList.add('tooltip');
+    // tooltip.textContent = movieExists ? 'Movie is already in favorites' : 'Movie added to favorites';
+
+    const clickedIcon = event.currentTarget;
+
+    clickedIcon.parentElement.appendChild(tooltip);
+    tooltip.classList.add('active');
+
+    setTimeout(() => {
+        tooltip.classList.remove('active');
+        tooltip.remove();
+    }, 2000);
 
     if (!movieExists) favorites.push(movie);
     localStorage.setItem('favoritesMovie', JSON.stringify(favorites));
+    clickedIcon.classList.toggle('saved', !movieExists);
+}
+
+function removeFavorite(movie) {
+    const favorites = JSON.parse(localStorage.getItem('favoritesMovie')) || [];
+    const updatedFavorites = favorites.filter((favMovie) => favMovie.id !== movie.id);
+
+    localStorage.setItem('favoritesMovie', JSON.stringify(updatedFavorites));
+    showFavorites();
+}
+
+function updateFavoriteIcon(movieId) {
+    const favorites = JSON.parse(localStorage.getItem('favoritesMovie')) || [];
+    const isBookmarked = favorites.some((favMovie) => favMovie.id === movieId);
+
+    const favoriteIcons = document.querySelectorAll('.fa-bookmark');
+    favoriteIcons.forEach(icon => {
+        const iconMovieId = icon.parentElement.dataset.movieId;
+        if (iconMovieId == movieId) {
+            icon.classList.toggle('saved', isBookmarked);
+        }
+    });
 }
 
 function showFavorites() {
@@ -197,12 +305,13 @@ function showFavorites() {
 
         const movieEl = document.createElement('div');
         movieEl.classList.add('movie');
+        
 
         movieEl.innerHTML = `
         <img src="${IMGPATH + poster_path}" alt="${title}">
         <div class="overview">
-        <h3>${title}</h3>
         <p class="expand"><i class="fa-solid fa-maximize"></i> Expand</p>
+        <h3>${title}</h3>
         </div>
         `;
 
@@ -210,13 +319,13 @@ function showFavorites() {
         removeFromFavoritesButton.classList.add('remove-button');
         const removeFavoritesIcon = document.createElement('i');
 
-        removeFromFavoritesButton.classList.add('fa', 'fa-bookmark', 'fa-regular');
+        removeFromFavoritesButton.classList.add('fa', 'fa-bookmark', 'fa-solid');
         removeFromFavoritesButton.classList.toggle('removeFavorite');
         
         removeFromFavoritesButton.appendChild(removeFavoritesIcon);
         
-        removeFromFavoritesButton.addEventListener('click', (event) => {
-            event.stopPropagation();
+        removeFromFavoritesButton.addEventListener('click', (e) => {
+            e.stopPropagation();
             removeFavorite(movie);
             removeFavoritesIcon.classList.toggle('removeFavorite');
         });
@@ -228,24 +337,6 @@ function showFavorites() {
             openMovieModal(movie.id);
         });
     });
-
-    function getClassByRate(vote) {
-        if (vote >= 8) {
-            return 'green';
-        } else if (vote >= 5) {
-            return 'orange';
-        } else {
-            return 'red';
-        }
-    }
-}
-
-function removeFavorite(movie) {
-    const favorites = JSON.parse(localStorage.getItem('favoritesMovie')) || [];
-    const updatedFavorites = favorites.filter((favMovie) => favMovie.id !== movie.id);
-
-    localStorage.setItem('favoritesMovie', JSON.stringify(updatedFavorites));
-    showFavorites();
 }
 
 closeBtn.addEventListener('click', () => {
@@ -264,14 +355,14 @@ function close() {
 const hamburgerLinks = document.querySelectorAll('.mainMenu li a');
 hamburgerLinks.forEach(link => {
     link.addEventListener('click', () => {
-        if (window.innerWidth <= 700) {
+        if (window.innerWidth <= 800) {
             close();
         }
     });
 });
 
 window.addEventListener('resize', () => {
-    if (window.innerWidth > 700) {
+    if (window.innerWidth > 820) {
         mainMenu.style.display = 'flex';
         mainMenu.style.top = 'initial';
     } else {
